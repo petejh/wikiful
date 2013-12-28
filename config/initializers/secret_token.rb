9fbb1108fa5b04323eb4f3923523f930c0164e2c
petejh <petejh@truly.fake>
2013-12-28 21:08:43 +0000

    Create the Wikiful project
    The Wikiful project is a minimally functional clone of Wikipedia, built
    following a Thinkful.com tutorial to learn Ruby On Rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikiful::Application.config.secret_key_base = '54621b0f4cc2d2c51dec790606cb63556e5a1b92df1ea0a0f619acec3fa236a5f22224d8c968e7aaa9d5b477c46a8da21726ce5f5e2343a23f5d715796a025c3'
