fc0d6a8505f61949df423e7e1f7de235e4d9c6b6
petejh <petejh@truly.fake>
2014-01-07 05:13:32 +0000

    Add a instance variable in ApplicationController to track the current user
    Assign a variable to track the current user and provide a helper method
    so that the application can use this information to tailor each view.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,9 @@ class ApplicationController < ActionController::Base
     @categories = Category.order(:name)
   end
   
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+  
 end
