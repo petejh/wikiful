474bd432ed20c4cf8782578a7c4ac6aab3d89d17
petejh <petejh@truly.fake>
2014-01-16 23:27:07 +0000

    Add view and controller methods to edit an Article
    Article#edit manages a view with form elements where and author can alter
    an existing article. Validations are performed by Article#update.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_filter :authorize, only: [:new]
+  before_filter :authorize, only: [:new, :edit]
 
   def index
     @articles = Article.order(updated_at: :desc).limit(25)
@@ -17,11 +17,26 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to @article
+      redirect_to @article, notice: 'New Article added!'
     else
       render 'new'
     end
   end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    @article.assign_attributes(article_params)
+    if @article.save
+      redirect_to @article, notice: 'Article successfully updated!'
+    else
+      render 'edit'
+    end
+  end
+  
   private
     def article_params
       params.require(:article).permit(:title, :content, :category_ids => [])
